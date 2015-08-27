25af1967e3edbf07d79a227fee316695193af7d5
MoreNiceJay <morenicejay@gmail.com>
2015-08-26 23:20:10 -0400

    Initial Commit

---
+++ NoNa_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoNa_App
+//
+//  Created by Lee Janghyup on 8/26/15.
+//  Copyright (c) 2015 jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

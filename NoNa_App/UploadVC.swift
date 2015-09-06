7617610f7eee66a176f745a9fe8b3f9f9523fc80
MoreNiceJay <morenicejay@gmail.com>
2015-09-06 13:00:06 -0400

    add some functions about picker view

--- NoNa_App/UploadVC.swift
+++ NoNa_App/UploadVC.swift
@@ -13,6 +13,13 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
 
     @IBOutlet weak var imageViewPreview: UIImageView!
     @IBOutlet weak var textFieldPreviewTitle: UITextField!
+    
+    var beginImage: CIImage!
+    var context: CIContext!
+    var filter: CIFilter!
+    var intensity: Float = 0.5
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,14 +40,32 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
         
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+    /*func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        //image.size = CGRect(origin: CGPoint, size: CGSize(width: 414, height: 191))
         imageViewPreview.image = image
         self.dismissViewControllerAnimated(true, completion: nil)
-        
+      */
+    func imagePickerViewController(picker: UIImagePickerController!,
+        didFinishPickingMediaWithInfo info: NSDictionary!) {
+            self.dismissViewControllerAnimated(true, completion: nil)
+            beginImage = CIImage(image: info[UIImagePickerControllerOriginalImage] as! UIImage)
+            self.updateImageView()
+    
+    
+    }
+    
+    
+    func updateImageView() {
+        filter.setValue(beginImage, forKey:kCIInputImageKey)
+        filter.setValue(intensity, forKey:kCIInputIntensityKey)
+        let output = context.createCGImage(filter.outputImage,
+            fromRect: filter.outputImage.extent())
+        let newImage = UIImage(CGImage: output)
+        self.imageViewPreview.image = newImage
     }
     
     @IBAction func uploadButtonTapped(sender: AnyObject) {
-        
+        println("업로드 시작")
         var imageText = textFieldPreviewTitle.text
         var previewIamge = imageViewPreview.image
         
